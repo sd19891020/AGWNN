2c392566e95e1190e2152658bedcb96dbafb0f1f
GWRer <1310649312@qq.com>
2024-09-29 12:24:03 +0800

    Delete Codes_and_Data_V3 directory

--- Codes_and_Data_V3/AGWNN/MakeGWih.m
+++
@@ -1,5 +0,0 @@
-function [GWih] = MakeGWih(iUV,hUV,bw)
-    dist = pdist2(iUV, hUV , 'euclidean');
-    GWih = exp(-(dist./bw).^2);
-end
-
--- Codes_and_Data_V3/Common/WriteCsvData.m
+++
@@ -1,9 +0,0 @@
-function [] = WriteCsvData(outPath,titles,vals)
-    
-    [~,col]=size(vals);
-    T=array2table(vals);
-    T.Properties.VariableNames(1:col)=titles;
-    writetable(T,outPath);
-
-end
-
